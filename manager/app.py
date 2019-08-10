94b4d6f6540685212e24dc52b9077d8546dcc188
visit.paridhi@gmail.com <visit.paridhi@gmail.com>
2019-08-11 09:00:53 +1000

    move app

--- manager/app.py
+++ manager/app.py
@@ -9,50 +9,52 @@ from manager.log_handler import LogHandler
 
 logger = logging.getLogger(__name__)
 
+app = Flask(__name__, instance_relative_config=True)
+# app.register_blueprint(manager.scheduler)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'manager.sqlite'),
+)
+#
+# if test_config is None:
+#     # load the instance config, if it exists, when not testing
+#     app.config.from_pyfile('config.py', silent=True)
+# else:
+#     # load the test config if passed in
+#     app.config.from_mapping(test_config)
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    # app.register_blueprint(manager.scheduler)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'manager.sqlite'),
-    )
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+db.init_app(app)
+model_handler.init_app(app)
 
-    # ensure the instance folder exists
+# apply the blueprints to the app
+#app.register_blueprint(auth.bp)
+
+@app.route('/start_scheduler')
+def start_scheduler():
     try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+        scheduler_obj = Scheduler()
+        scheduler_obj.start()
+    except BaseException as e:
+        LogHandler("This is my exception").error()
 
-    db.init_app(app)
-    model_handler.init_app(app)
-    # apply the blueprints to the app
-    app.register_blueprint(auth.bp)
+    return 'Scheduler started'
 
-    @app.route('/start_scheduler')
-    def start_scheduler():
-        try:
-            scheduler_obj = Scheduler()
-            scheduler_obj.start()
-        except BaseException as e:
-            LogHandler("This is my exception").error()
+@app.route('/start_manager')
+def start_manager():
+    return 'SUCCESS'
 
-        return 'Scheduler started'
+@app.route('/api')
+def api():
+    return jsonify(username="aa",
+                   email="bb")
 
-    @app.route('/start_manager')
-    def start_manager():
-        return 'SUCCESS'
 
-    @app.route('/api')
-    def api():
-        return jsonify(username="aa",
-                       email="bb")
-    return app
+if __name__ == '__main__':
+    app.run(debug=True)
 
