be8969e95def650ff60cc24d769015c1a3d731e6
Paridhi Jha <paridhi.jha@nab.com.au>
2019-08-18 15:58:50 +1000

    add boto cloudwatch

--- mmf/model_handler.py
+++ mmf/model_handler.py
@@ -1,12 +1,19 @@
 import importlib
 import logging
 import click
+import boto3
+import os
+
 from flask.cli import with_appcontext
 from datetime import datetime
 from mmf.log_handler import LogHandler
 from mmf.exception import ModelNotFoundError, ModelRunFailedError
 
 logger = logging.getLogger(__name__)
+# Create CloudWatch client
+cloudwatch = boto3.client('cloudwatch', region_name='ap-southeast-2',
+                          aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+                          aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"))
 
 def init_app(app):
     app.cli.add_command(run_model_command)
@@ -22,21 +29,43 @@ def run_model(model) -> dict:
         logger.error('Model name empty in run_model()')
         raise ModelNotFoundError
 
+    model_execution_status = {'execution_time': datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
     try:
         model_main = importlib.import_module('mmf' + '.' + model + '.' + 'main')
     except ModuleNotFoundError as e:
         logger.error(f'Module missing for model = {model}', e)
-        return { 'status': 'fail'}
+        model_execution_status['status'] = 'fail'
+        return model_execution_status
 
     try:
-        model_main.run_model()
+        model_result = model_main.run_model()
     except ModelRunFailedError as e:
         logger.error(f'Model failed to run for model = {model}', e)
-        return { 'status': 'fail'}
+        model_execution_status['status'] = 'fail'
+        return model_execution_status
 
-    result_json = {'execution_time': datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 'status': 'success'}
-    logger.debug(f' Running {model}, execution result is {result_json}')
-    return result_json
+    model_execution_status['status'] = 'pass'
+    model_execution_status['model_result'] = model_result
+    logger.debug(f' Running {model}, execution result is {model_execution_status}')
+
+    # Put custom metrics
+    cloudwatch.put_metric_data(
+        MetricData=[
+            {
+                'MetricName': model,
+                'Dimensions': [
+                    {
+                        'Name': 'UNIQUE_PAGES',
+                        'Value': 'URLS'
+                    },
+                ],
+                'Unit': 'None',
+                'Value': model_execution_status['model_result']['confidence']
+            },
+        ],
+        Namespace='MMF'
+    )
+    return model_execution_status
 
 
 @click.command('run-model')
